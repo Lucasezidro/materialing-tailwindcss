f795c386ddaddea4409420bc552c35c75477b927
lucasezidro <lucasezidro7@gmail.com>
2024-06-16 13:19:00 -0300

    add dark theme for form

--- src/components/form/input.tsx
+++ src/components/form/input.tsx
@@ -12,7 +12,7 @@ type InputControlProps = ComponentProps<'input'>
 export function Control(props: InputControlProps) {
   return (
     <input
-      className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600 outline-none dark:placeholder:zinc-400 dark:text-zinc-100"
+      className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600 outline-none dark:placeholder-zinc-400 dark:text-zinc-100"
       {...props}
     />
   )
